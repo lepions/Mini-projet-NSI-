aa8d2cb2551a70cf9839e20cebfd62aaddc11f5a
lepions <45270812+lepions@users.noreply.github.com>
2023-01-17 23:04:07 +0100

    Update mini_projet.py

--- mini_projet.py
+++ mini_projet.py
@@ -15,27 +15,26 @@ def encodage2(phrase,decalage):
             #correspondant et l'ajoute à la variable liste
     return liste 
 
-def encodage(phrase,decalage):
-    liste=""
-    for i in range(len(phrase)):
-      num = ord(phrase[i])
-      resultat = int(decalage) + 34
-      lettre = int(num)+int(decalage)
-      if lettre > 126 and lettre <161:
-        liste+=chr(ord(phrase[i])+resultat)
-      else:
-        liste+=chr(ord(phrase[i])+decalage)
-    return liste
-
-
-def decodage(phrase,decalage):
-  liste=""
+def encodage(phrase, decalage):
+  liste = ""
   for i in range(len(phrase)):
-    lettre = int(ord(phrase[i]))+int(decalage)
-    if lettre > 126 and lettre <161:
-      liste+=chr(ord(phrase[i])-(int(decalage) + 34))
+    resultat = int(decalage) + 34#calcule la valeur de décalage avec un ajout de 34 (pour eviter les caractères vides entre 127 et 160)
+    lettre = int(ord(phrase[i])) + int(decalage) #calcule la valeur ASCII du caractère encodé en ajoutant la valeur de décalage à la valeur ASCII de l'indice de caractère actuel de la variable 'phrase'.
+    if lettre > 126 and lettre < 161: #vérifie si la variable 'lettre' est supérieure à 126 et inférieure à 161. Si cette condition est vraie, cela signifie que le caractère encodé est un caractère non imprimable ou étendu.
+      liste += chr(ord(phrase[i]) + resultat) #ajoute le caractère correspondant à la valeur ASCII qui résulte de l'ajout de la variable 'resultat' à l'indice du caractère actuel de la variable 'phrase', à la variable 'liste'.
     else:
-      liste+=chr(ord(phrase[i])-decalage)
+      liste += chr(ord(phrase[i]) + decalage) #ajoute le caractère correspondant à la valeur ASCII qui résulte de l'ajout de la variable 'decalage' à l'indice du caractère actuel de la variable 'phrase', à la variable 'liste'.
+  return liste
+
+
+def decodage(phrase, decalage):
+  liste = ""
+  for i in range(len(phrase)):
+      lettre = int(ord(phrase[i])) - int(decalage) #prend la valeur ASCII du caractère actuel dans la variable 'phrase', soustrait la valeur actuelle de 'decalage' et assigne le résultat à une variable appelée 'lettre'.
+      if lettre > 126 and lettre < 161: #vérifie si la variable 'lettre' est supérieure à 126 et inférieure à 161. Si cette condition est vraie, cela signifie que le caractère décodé est un caractère non imprimable ou étendu.
+        liste += chr(ord(phrase[i]) - decalage - 34) # ajoute le caractère correspondant à la valeur ASCII qui résulte de la soustraction de 34 à la valeur actuelle de 'decalage' et de l'indice du caractère actuel de la variable 'phrase', à la variable 'liste'
+      else:
+        liste += chr(ord(phrase[i]) - decalage) # ajoute le caractère correspondant à la valeur ASCII qui résulte de la soustraction de l'indice du caractère actuel de la variable 'phrase' à la valeur actuelle de 'decalage', à la variable 'liste'.
   return liste
 
 
@@ -54,25 +53,35 @@ def décodage(phrase,decalage):
     return liste
 
 def decodage_brut(phrase):
-    liste="" #initialise une variable chaîne vide appelée 'liste' qui sera utilisée pour stocker la phrase décodée.
-    fragment1="" # initialise une variable chaîne vide appelée 'fragment1' qui sera utilisée pour stocker des parties de la phrase décodée.
-    decalage=0 #initialise une variable appelée 'decalage' à 0. Cette variable sera utilisée pour contrôler le nombre de décalages appliqués à chaque caractère de la phrase encodée.
-    dico = open(r"mini projet\Français.txt") #ouvre un fichier texte situé au chemin spécifié et affecte son contenu à la variable 'dico'. Ce fichier texte sera utilisé 
-    #comme un dictionnaire pour vérifier si la phrase décodée contient des mots valides.
-    read = dico.read() # lit le contenu du fichier 'dico' et l'affecte à la variable 'read'.
-    while decalage <= 256: #démarre une boucle while qui va itérer 256 fois. La boucle continuera jusqu'à ce que la variable 'decalage' soit supérieure à 256.
-        for i in range(len(phrase)): #démarre une boucle for qui va itérer sur chaque caractère de la variable 'phrase'.
-            liste+=str(chr(ord(phrase[i])-decalage)) #prend la valeur ASCII du caractère courant dans la variable 'phrase', soustrait la valeur courante de 'decalage', convertit le résultat en un caractère, 
-            #et l'ajoute à la variable 'liste'. Cette ligne applique le décryptage du chiffre Caesar.
-        decalage=decalage+1 # incrémente la variable 'decalage' de 1.
-        fragment1 = liste.split(" ") #divise la variable 'liste' en une liste de mots en utilisant le caractère espace comme délimiteur, et affecte le résultat à la variable 'fragment1'.
-        
-        for mot in fragment1: #démarre une boucle for qui va itérer à travers chaque mot de la liste 'fragment1'.
-            if mot in read: #vérifie si le mot courant dans la liste 'fragment1' est présent dans la variable 'read' (i.e., le contenu du fichier dictionnaire).
-                print("La phrase décoder est :",liste,", la clé de chiffrement est :""",decalage-1) # imprime la phrase décodée et la clé utilisée pour la déchiffrer si un mot valide est trouvé dans la phrase décodée.
-                quit() #arrête l'exécution du script lorsqu'un mot valide est trouvé.
-        liste="" # réinitialise la variable 'liste' à une chaîne vide afin qu'elle puisse être réutilisée à la prochaine itération de la boucle while.
+  liste = ""  #initialise une variable chaîne vide appelée 'liste' qui sera utilisée pour stocker la phrase décodée.
+  fragment1 = ""  # initialise une variable chaîne vide appelée 'fragment1' qui sera utilisée pour stocker des parties de la phrase décodée.
+  decalage = 0  #initialise une variable appelée 'decalage' à 0. Cette variable sera utilisée pour contrôler le nombre de décalages appliqués à chaque caractère de la phrase encodée.
+  dico = open(
+    r"mini_projet\Français.txt"
+  )  #ouvre un fichier texte situé au chemin spécifié et affecte son contenu à la variable 'dico'. Ce fichier texte sera utilisé
+  #comme un dictionnaire pour vérifier si la phrase décodée contient des mots valides.
+  read = dico.read(
+  )  # lit le contenu du fichier 'dico' et l'affecte à la variable 'read'.
+  while decalage <= 256:  #démarre une boucle while qui va itérer 256 fois. La boucle continuera jusqu'à ce que la variable 'decalage' soit supérieure à 256.
+    for i in range(len(phrase)):
+      lettre = int(ord(phrase[i])) - int(decalage) #prend la valeur ASCII du caractère actuel dans la variable 'phrase', soustrait la valeur actuelle de 'decalage' et assigne le résultat à une variable appelée 'lettre'.
+      if lettre > 126 and lettre < 161: #vérifie si la variable 'lettre' est supérieure à 126 et inférieure à 161. Si cette condition est vraie, cela signifie que le caractère décodé est un caractère non imprimable ou étendu.
+        liste += chr(ord(phrase[i]) - decalage - 34) # ajoute le caractère correspondant à la valeur ASCII qui résulte de la soustraction de 34 à la valeur actuelle de 'decalage' et de l'indice du caractère actuel de la variable 'phrase', à la variable 'liste'
+      else:
+        liste += chr(ord(phrase[i]) - decalage) # ajoute le caractère correspondant à la valeur ASCII qui résulte de la soustraction de l'indice du caractère actuel de la variable 'phrase' à la valeur actuelle de 'decalage', à la variable 'liste'.
+    decalage = decalage + 1  # incrémente la variable 'decalage' de 1.
+    fragment1 = liste.split(
+      " "
+    )  #divise la variable 'liste' en une liste de mots en utilisant le caractère espace comme délimiteur, et affecte le résultat à la variable 'fragment1'.
 
+    for mot in fragment1:  #démarre une boucle for qui va itérer à travers chaque mot de la liste 'fragment1'.
+      if mot in read:  #vérifie si le mot courant dans la liste 'fragment1' est présent dans la variable 'read' (i.e., le contenu du fichier dictionnaire).
+        print(
+          "La phrase décoder est :", liste, ", la clé de chiffrement est :"
+          "", decalage - 1
+        )  # imprime la phrase décodée et la clé utilisée pour la déchiffrer si un mot valide est trouvé dans la phrase décodée.
+        quit()  #arrête l'exécution du script lorsqu'un mot valide est trouvé.
+    liste = ""  # réinitialise la variable 'liste' à une chaîne vide afin qu'elle puisse être réutilisée à la prochaine itération de la boucle while.
   
 
     
